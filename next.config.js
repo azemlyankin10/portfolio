df37c59a12c61e46116d5d968cf527a2e9f6be4e
Andrey Zemlyankin <azemlyankin10@gmail.com>
2022-06-18 15:00:50 +0300

    fix bugs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    API_URL: 'https://azemlyankin-portfolio-igpl1hn1j-azemlyankin10.vercel.app'
-  }
+  // env: {
+  //   API_URL: 'http://localhost:3000'
+  // }
 }
 
 module.exports = nextConfig
