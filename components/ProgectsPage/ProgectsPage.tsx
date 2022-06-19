c5635b6e358fac2af308002168b198fb32389a69
Andrey Zemlyankin <azemlyankin10@gmail.com>
2022-06-19 12:22:33 +0300

    add select on progect page

--- components/ProgectsPage/ProgectsPage.tsx
+++ components/ProgectsPage/ProgectsPage.tsx
@@ -1,4 +1,5 @@
-import { Container } from 'react-bootstrap'
+import { useEffect, useState } from 'react'
+import { Col, Container, Form, Row } from 'react-bootstrap'
 import { useRecoilValue } from 'recoil'
 import { isCardOpenState } from '../../Store/Atoms'
 import { Card } from './Card/Card'
@@ -17,11 +18,52 @@ type typeProgect = {
 
 export const ProgectsPageComponent = ({ data }: { data: typeProgect[] }) => {
   const isOpen = useRecoilValue(isCardOpenState)
+  const [progects, setProgects] = useState(data)
+  const [selectValue, setSelectValue] = useState('all')
+
+  useEffect(() => {
+    switch (selectValue) {
+      case 'all':
+        setProgects(data)
+        break
+      case 'landing':
+        setProgects(data.filter(el => el.type === 'Landing page'))
+        break
+      case 'multiple':
+        setProgects(data.filter(el => el.type === 'Multipage'))
+        break
+      case 'spa':
+        setProgects(data.filter(el => el.type === 'SPA'))
+        break
+      default:
+        setProgects(data)
+        break
+    }
+  }, [data, selectValue])
+
+  const selectHandler = (e: any) => { setSelectValue(e.target.value) }
 
   return (
     <Container>
-      <div className={`cards ${isOpen ? 'showing' : ''} pt-5`}>
-        {data.map((el, index) => (
+      <Row className='pt-4 pt-lg-5 mt-lg-3'>
+        <Col lg={9}>
+          <h2 className='fs-1'>Portfolio</h2>
+        </Col>
+        <Col lg={3}>
+          <Form.Select 
+            className='ms-auto'
+            onChange={selectHandler}
+            value={selectValue}
+          >
+            <option value='all'>All</option>
+            <option value='landing'>Landing page</option>
+            <option value='multiple'>Multiple page</option>
+            <option value='spa'>SPA</option>
+          </Form.Select>
+        </Col>
+      </Row>
+      <div className={`cards ${isOpen ? 'showing' : ''} py-5`}>
+        {progects.map((el, index) => (
           <Card key={index}
             title={el.name}
             subtitle={el.type}
